5647dd5e47549e72a61ec5eb1b282318c23b89ac
josdavidmo <pabarragang@outlook.es>
2018-10-06 14:29:58 -0500

    add api rest services

--- beitech_store/settings.py
+++ beitech_store/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'invoice'
+    'rest_framework',
+    'invoice.apps.InvoiceConfig'
 ]
 
 MIDDLEWARE = [
--- invoice/views.py
+++ invoice/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from invoice.models import (AvailableProduct, Customer, Order, OrderDetail,
+                            Product)
+from invoice.serializers import (AvailableProductSerializer,
+                                 CustomerSerializer, OrderDetailSerializer,
+                                 OrderSerializer, ProductSerializer)
+
+
+class ProductList(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class CustomerList(generics.ListCreateAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+
+class CustomerDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = ProductSerializer
+
+
+class AvailableProductList(generics.ListCreateAPIView):
+    queryset = AvailableProduct.objects.all()
+    serializer_class = AvailableProductSerializer
+
+
+class AvailableProductDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = AvailableProduct.objects.all()
+    serializer_class = AvailableProductSerializer
+
+
+class OrderList(generics.ListCreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class OrderDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class OrderDetailList(generics.ListCreateAPIView):
+    queryset = OrderDetail.objects.all()
+    serializer_class = OrderDetailSerializer
+
+
+class OrderDetailDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = OrderDetail.objects.all()
+    serializer_class = OrderDetailSerializer
