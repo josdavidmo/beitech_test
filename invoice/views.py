de6bb5cf47d9debfe69c2506fc25d952e66cbd86
josdavidmo <pabarragang@outlook.es>
2018-10-06 20:34:59 -0500

    Allow to list order in html

--- invoice/views.py
+++ invoice/views.py
@@ -1,10 +1,11 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
 from rest_framework.renderers import BrowsableAPIRenderer, JSONRenderer
+from rest_framework.response import Response
 
-from invoice.models import Order
+from invoice.models import Customer, Order
 from invoice.renderers import DatatablesRenderer
-from invoice.serializers import OrderSerializer
+from invoice.serializers import CustomerSerializer, OrderSerializer
 
 
 class OrderList(generics.ListCreateAPIView):
@@ -14,10 +15,17 @@ class OrderList(generics.ListCreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
     filter_backends = (DjangoFilterBackend,)
-    filter_fields = {'customer': ['exact'], 'date': ['range']}
+    filter_fields = {'customer': ['exact'], 'date': ['range','exact']}
     renderer_classes = (JSONRenderer, BrowsableAPIRenderer, DatatablesRenderer)
-
 
 class OrderDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+
+class CustomerList(generics.ListCreateAPIView):
+    """
+    List all customer.
+    """
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+    filter_backends = (DjangoFilterBackend,)
