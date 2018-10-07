0bf5ca41d6412f5d6cf1d34eb52dcadab023cdac
José David Moreno Posada <josdavidmo@gmail.com>
2018-10-07 11:56:02 -0500

    add POST method in Order service

--- invoice/urls.py
+++ invoice/urls.py
@@ -9,7 +9,6 @@ from invoice import views
 app_name = 'invoice'
 urlpatterns = [
     re_path(r'^order/$', views.OrderList.as_view()),
-    re_path(r'^order/(?P<pk>[0-9]+)/$', views.OrderDetail.as_view()),
     re_path(r'^customer/$', views.CustomerList.as_view()),
 ]
 
